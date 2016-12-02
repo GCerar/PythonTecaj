6dd63a645f48be1832a8afd70fa9c72415ed6a41
Gregor Cerar <grega90@gmail.com>
2016-12-02 07:54:03 +0100

    Dodal GUI primer s TKinter

---
+++ LTFE/primeri/python_gui/s_tk.py
@@ -0,0 +1,72 @@
+"""
+Preprosta demo aplikacija za Python tečaj. Narejena je s Tk/TKinter
+"""
+
+import tkinter as tk
+import requests
+
+url = 'http://trola.si/'
+headers = {'Accept': 'application/json'}
+
+
+class Application(tk.Frame):
+    
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.label_postaja = tk.Label(self, text='Postaja:')
+        self.label_postaja.grid(row=0, column=0)
+        
+        self.postaja = tk.Entry(self)
+        self.postaja.grid(row=0, column=1)
+        
+        self.find = tk.Button(self, text='Poišči', command=self.fetch_data)
+        self.find.grid(row=0, column=2)
+        
+        self.quit = tk.Button(self, text="Izhod", command=root.destroy)
+        self.quit.grid(row=0, column=3)
+        
+        self.seznam = tk.Listbox(self, width=40)
+        self.seznam.grid(row=1, columnspan=4)
+        
+        
+    def fetch_data(self, event=None):
+        ime_postaje = self.postaja.get()
+        res = requests.get(url + ime_postaje, headers=headers)
+        data = res.json()
+        busi = []
+        for postaja in data['stations']:
+            for bus in postaja['buses']:
+                busi.append('{} ({}): {}'.format(bus['direction'], bus['number'], bus['arrivals']))
+                
+        #self.seznam.delete(0, -1)
+        for i, bus in enumerate(busi):
+            self.seznam.insert(i, bus)
+        
+
+        
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('Get-a-bus')
+    root.geometry('250x150')
+    Application(root)
+    root.mainloop()
+        
+        
+
+# inicializacija podatkovne baze
+#conn = sqlite3.connect(':memory:')
+#c = conn.cursor()
+
+#c.execute('CREATE TABLE osebe (ime TEXT, starost INTEGER)')
+#conn.commit()
+
+
+#root = tk.Tk()
+#app = Application(master=root)
+#app.mainloop()
